698a577cf213330e7f0f43ef98eace7d2cc8235d
Georg Habeck <geha00@gmail.com>
2017-10-19 15:28:35 +0200

    Remove mouse functionality

--- keyboards/planck/keymaps/geha/rules.mk
+++ keyboards/planck/keymaps/geha/rules.mk
@@ -2,7 +2,7 @@ ifndef QUANTUM_DIR
 	include ../../../../Makefile
 endif
 
-MOUSEKEY_ENABLE = yes     # Mouse keys(+4700)
+MOUSEKEY_ENABLE = no      # Mouse keys(+4700)
 EXTRAKEY_ENABLE = yes     # Audio control and System control(+450)
 AUDIO_ENABLE = yes        # Audio output on port C6
 UNICODE_ENABLE = no       # Unicode
