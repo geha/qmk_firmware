c6299b5b24acb7e2d357d8097624fb967474bd7f
Georg Habeck <geha00@gmail.com>
2017-11-02 13:39:22 +0100

    Mirror Alt and GUI modifiers on right side

--- keyboards/planck/keymaps/geha/keymap.c
+++ keyboards/planck/keymaps/geha/keymap.c
@@ -61,14 +61,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Enter |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Ctrl |  GUI |  Alt | Media|Lower | Bksp |Space |Raise | Move |      | Umlt | Ctrl |
+ * | Ctrl |  GUI |  Alt | Media|Lower | Bksp |Space |Raise | Move |  Alt |  GUI | Ctrl |
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = {
-  {KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,        KC_BSPC},
-  {KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,     KC_QUOT},
-  {KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,     KC_ENT },
-  {KC_LCTL, KC_LGUI, KC_LALT, MEDIA,   LOWER,   KC_BSPC, KC_SPC,  RAISE,   MOVE,    _______, TD(TD_UMLS), KC_RCTL}
+  {KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC},
+  {KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT},
+  {KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT },
+  {KC_LCTL, KC_LGUI, KC_LALT, MEDIA,   LOWER,   KC_BSPC, KC_SPC,  RAISE,   MOVE,    KC_RALT, KC_RGUI, KC_RCTL}
 },
 
 /* Colemak
@@ -79,14 +79,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  |   K  |   M  |   ,  |   .  |   /  |Enter |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Ctrl |  GUI |  Alt | Media|Lower | Bksp |Space |Raise | Move |      | Umlt | Ctrl |
+ * | Ctrl |  GUI |  Alt | Media|Lower | Bksp |Space |Raise | Move |  Alt |  GUI | Ctrl |
  * `-----------------------------------------------------------------------------------'
  */
 [_COLEMAK] = {
-  {KC_ESC,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN,     KC_BSPC},
-  {KC_TAB,  KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_H,    KC_N,    KC_E,    KC_I,    KC_O,        KC_QUOT},
-  {KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,     KC_ENT },
-  {KC_LCTL, KC_LGUI, KC_LALT, MEDIA,   LOWER,   KC_BSPC, KC_SPC,  RAISE,   MOVE,    _______, TD(TD_UMLS), KC_RCTL}
+  {KC_ESC,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSPC},
+  {KC_TAB,  KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT},
+  {KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT },
+  {KC_LCTL, KC_LGUI, KC_LALT, MEDIA,   LOWER,   KC_BSPC, KC_SPC,  RAISE,   MOVE,    KC_RALT, KC_RGUI, KC_RCTL}
 },
 
 /* Lower
