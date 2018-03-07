431b963d463b716bef5f6fcbd246606160a124e4
Georg Habeck <geha00@gmail.com>
2018-03-07 13:23:26 +0100

    Backspace and delete via space bar on lower and raise layers

--- keyboards/preonic/keymaps/geha/config.h
+++ keyboards/preonic/keymaps/geha/config.h
@@ -6,12 +6,7 @@
 #ifdef AUDIO_ENABLE
     // #define STARTUP_SONG SONG(PREONIC_SOUND)
     #define STARTUP_SONG SONG(NO_SOUND)
-
-    /*#define DEFAULT_LAYER_SONGS { SONG(QWERTY_SOUND), \
-                                  SONG(COLEMAK_SOUND), \
-                                  SONG(DVORAK_SOUND) \
-                                }
-    */
+    #define GOODBYE_SONG SONG(NO_SOUND)
 #endif
 
 #define MUSIC_MASK (keycode != KC_NO)
