376508ade25095d1caa26ff3bbb01bd33b5cff26
Georg Habeck <geha00@gmail.com>
2018-07-13 16:00:04 +0200

    Replace right GUI key with Ctrl+Alt modifier

--- keyboards/preonic/keymaps/geha/keymap.c
+++ keyboards/preonic/keymaps/geha/keymap.c
@@ -44,6 +44,7 @@ enum preonic_keycodes {
 #define KC_SS   RALT(KC_S)
 #define KC_EURO RALT(KC_5)
 #define KC_DEGR RALT(KC_COLN)
+#define KC_LCA  LCA(KC_NO)
 #define LT_NMPD LT(_NUMPAD, KC_TAB)
 #define LT_MOVE LT(_MOVEMENT, KC_COMM)
 #define M_FLOAT LCTL(LSFT(KC_9))
@@ -60,7 +61,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Enter |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Ctrl |  GUI |  Alt | Move |Lower |    Space    |Raise | Move |  Alt |  GUI | Ctrl |
+ * | Ctrl |  GUI |  Alt | Move |Lower |    Space    |Raise | Move |  Alt |CtlAlt| Ctrl |
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = {
@@ -68,7 +69,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   {KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS},
   {LT_NMPD, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT},
   {KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT },
-  {KC_LCTL, KC_LGUI, KC_LALT, MOVE,    LOWER,   KC_SPC,  KC_SPC,  RAISE,   MOVE,    KC_RALT, KC_RGUI, KC_RCTL}
+  {KC_LCTL, KC_LGUI, KC_LALT, MOVE,    LOWER,   KC_SPC,  KC_SPC,  RAISE,   MOVE,    KC_RALT, KC_LCA,  KC_RCTL}
 },
 
 /* Colemak Mod-DH
@@ -81,7 +82,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   D  |   V  |   K  |   H  |   ,  |   .  |   /  |Enter |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Ctrl |  GUI |  Alt | Move |Lower |    Space    |Raise | Move | Alt  | GUI  | Ctrl |
+ * | Ctrl |  GUI |  Alt | Move |Lower |    Space    |Raise | Move | Alt  |CtlAlt| Ctrl |
  * `-----------------------------------------------------------------------------------'
  */
 [_COLEMAK_MDH] = {
@@ -89,7 +90,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   {KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSLS},
   {LT_NMPD, KC_A,    KC_R,    KC_S,    KC_T,    KC_G,    KC_M,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT},
   {KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,    KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT },
-  {KC_LCTL, KC_LGUI, KC_LALT, MOVE,    LOWER,   KC_SPC,  KC_SPC,  RAISE,   MOVE,    KC_RALT, KC_RGUI, KC_RCTL}
+  {KC_LCTL, KC_LGUI, KC_LALT, MOVE,    LOWER,   KC_SPC,  KC_SPC,  RAISE,   MOVE,    KC_RALT, KC_LCA,  KC_RCTL}
 },
 
 /* Lower
